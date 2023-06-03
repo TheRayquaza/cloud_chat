aed28c57bf2aa70a8732b84cb34ca17d16d7db16
TheRayquaza <mateo.lelong@gmail.com>
2023-06-03 20:45:12 +0800

    Testing deployement for EC2 instance [2]

--- client/install.sh
+++ client/install.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 echo "Installing nginx"
 sudo apt install nginx
 
 echo "Adding static files to /var/www/html/loqui_chat"
 sudo mkdir /var/www/html/loqui_chat
 sudo cp -r dist/* /var/www/html/loqui_chat
+sudo mv /var/www/html/loqui_chat/assets/* /var/www/html/loqui_chat
+sed -i 's/assets\/index-\(.*\.js\)/index-\1/g; s/assets\/index-\(.*\.css\)/index-\1/g' /var/www/html/index.html
 
-echo ""
-
+echo "Changing nginx configuration"
+sudo cp server_conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/server_conf /etc/nginx/sites-enabled/server_conf
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "1. Updating packages"
 sudo apt -y update
 sudo apt -y full-upgrade
