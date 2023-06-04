2e65b6136d8326a333784edd007226f5e277701b
TheRayquaza <mateo.lelong@gmail.com>
2023-06-04 19:22:39 +0800

    Testing deployement for EC2 instance [8]

--- server/dist/middlewares/log_exit.js
+++ server/dist/middlewares/log_exit.js
@@ -2,6 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const logger_1 = require("../logger");
 const log_exit = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://loqui-chat.xyz');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.on('finish', () => logger_1.middleware_logger.info(`${req.method} ${req.originalUrl} ${res.statusCode}}`));
     next();
 };
