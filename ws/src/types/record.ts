a626c16a3c3c789d17ec4d70208a01499c00382b
TheRayquaza <mateo.lelong@gmail.com>
2023-09-17 13:41:33 +0200

    Fixing project and test suite [2]

--- ws/src/types/record.ts
+++
@@ -1,9 +0,0 @@
-import message from "./message";
-import user from "./user";
-import conversation from "./conversation";
-
-export type record = {
-    user : user,
-    conversation : conversation,
-    messages : message[]
-}
